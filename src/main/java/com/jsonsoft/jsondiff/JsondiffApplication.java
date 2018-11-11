85536918e369814ee23adcb9fac405989663e5ed
fred <fredrischter@gmail.com>
2018-11-11 15:46:28 -0200

    Base project, improvements on the .gitignore

---
+++ src/main/java/com/jsonsoft/jsondiff/JsondiffApplication.java
@@ -0,0 +1,12 @@
+package com.jsonsoft.jsondiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsondiffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsondiffApplication.class, args);
+	}
+}
